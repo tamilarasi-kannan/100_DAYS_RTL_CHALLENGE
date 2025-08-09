74aed6811803de1e468bcf26a5a66e9ff16d6c6e
Tamilarasi Kannan <115526719+tamilarasi-kannan@users.noreply.github.com>
2025-08-09 22:51:18 +0530

    Create testbench.v

---
+++ Day 5 - (Full subtractor)/testbench.v
@@ -0,0 +1,14 @@
+module full_subtractor_tb();
+  reg a,b,c;
+  wire diff,borrow;
+  
+  full_subtractor dut(.a(a),.b(b),.c(c),.diff(diff),.borrow(borrow));
+  
+  initial begin
+    repeat(15) begin
+      {a,b,c} = $random();
+      #10;
+      $display("a = %b | b = %b | c = %b | diff = %b | borrow = %b",a,b,c,diff,borrow);
+    end
+  end
+endmodule
